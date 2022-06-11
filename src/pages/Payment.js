59598503e998e34b3ba9686e99323a7e33bb095d
QuocThinhPham <thinhphamk8@gmail.com>
2022-06-11 20:42:11 +0700

    ui

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -2,7 +2,13 @@ import { Wrapper } from 'components/Layout';
 import React from 'react';
 
 const Payment = () => {
-   return <Wrapper>Payment</Wrapper>;
+   return (
+      <Wrapper>
+         <div className='w-full h-[calc(100% - 122px)] flex items-center justify-center'>
+            Not Ready To Use
+         </div>
+      </Wrapper>
+   );
 };
 
 export default Payment;
